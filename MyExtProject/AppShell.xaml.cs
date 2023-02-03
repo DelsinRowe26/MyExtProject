c262b008a23c37234fccb3890a63f89db4a37d4c
DelsinRowe26 <ilya.gritchin20020909@gmail.com>
2023-02-03 14:10:09 +0700

    Добавьте файлы проекта.

---
+++ MyExtProject/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyExtProject;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MyExtProject/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace MyExtProject;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void BtnOne_Click(object sender, EventArgs e)
+	{
+		imgWall.Source = "pika.png";
+		lbTxt.Text = "Pikachu";
+	}
+
+	private void BtnTwo_Clicked(object sender, EventArgs e)
+	{
+		imgWall.Source = "txt.png";
+		lbTxt.Text = "Text";
+	}
+
+	private void BtnThree_Clicked(object sender, EventArgs e)
+	{
+		imgWall.Source = "logo.png";
+		lbTxt.Text = "Logo";
+	}
+}
+
